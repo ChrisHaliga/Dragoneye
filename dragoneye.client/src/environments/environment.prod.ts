38086243e3e2a3027eb523d2a18d03f0d9502962
Chris Haliga <Christianhaliga@gmail.com>
2025-09-16 22:02:53 -0500

    prod env for frontend

--- dragoneye.client/src/environments/environment.prod.ts
+++ dragoneye.client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com',
+  apiUrl: 'https://dragoneye.azurewebsites.net/',
   auth: {
-    domain: 'YOUR_AUTH0_DOMAIN', // Replace with your Auth0 domain
-    clientId: 'YOUR_AUTH0_CLIENT_ID', // Replace with your Auth0 client ID
-    redirectUri: 'https://your-production-url.com', // Your production app URL
-    audience: 'YOUR_AUTH0_AUDIENCE' // Replace with your Auth0 API identifier
+    domain: 'dev-czez0gt6kknfxpso.us.auth0.com',
+    clientId: 'BbggPGhrKJXfDlEev83KopcR7S8PQ5U6',
+    redirectUri: 'https://localhost:62251',
+    audience: 'dragoneye'
   }
 };
--- dragoneye.client/src/environments/environment.ts
+++ dragoneye.client/src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:7147',
   auth: {
-    domain: 'dev-czez0gt6kknfxpso.us.auth0.com', // Replace with your Auth0 domain
-    clientId: 'BbggPGhrKJXfDlEev83KopcR7S8PQ5U6', // Replace with your Auth0 client ID
-    redirectUri: 'https://localhost:62251', // Your app URL
-    audience: 'dragoneye' // Replace with your Auth0 API identifier
+    domain: 'dev-czez0gt6kknfxpso.us.auth0.com',
+    clientId: 'BbggPGhrKJXfDlEev83KopcR7S8PQ5U6', 
+    redirectUri: 'https://localhost:62251',
+    audience: 'dragoneye'
   }
 };
